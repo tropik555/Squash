bfb8d659b3d0a8b18a70f66c92e4cf906bb1e59d
tropik555 <yevhen.levchenko@nure.ua>
2021-09-20 13:05:52 +0300

    first commit

---
+++ src/main/java/com/example/squash/SquashApplication.java
@@ -0,0 +1,13 @@
+package com.example.squash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquashApplication.class, args);
+    }
+
+}
